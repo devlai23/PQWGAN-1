231555ba32a005204bb3c2ed44ea37814b5fdf36
devlai23 <57195223+devlai23@users.noreply.github.com>
2024-10-09 15:09:07 -0400

    images

--- train.py
+++ train.py
@@ -155,4 +155,4 @@ if __name__ == "__main__":
     
     train(args.classes, args.dataset, args.patches, args.layers, args.qubits, args.batch_size, args.out_folder, args.checkpoint, args.randn, tuple(args.patch_shape), args.qcritic)
 
-#python3 train.py --classes 01 --dataset mnist --patches 1 --layers 153 --qubits 10 --batch_size 32 --out_folder results_1_patch
+#python train.py --classes 01 --dataset mnist --patches 28 --layers 10 --qubits 5 --batch_size 25 --out_folder results_28_patch
